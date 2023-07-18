f3ec25f37a56bedbb02b710e338117a3a4fafd5d
Carlos Mario Gonzalez Meza <119520751+xcarlosg@users.noreply.github.com>
2023-07-17 21:02:04 -0500

    commit inicial

---
+++ script.js
@@ -0,0 +1,71 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("php");
+        habilidades[2].classList.add("htmlcss");
+        habilidades[3].classList.add("sql");
+        habilidades[4].classList.add("analisisdato");
+        habilidades[5].classList.add("planeacion");
+        habilidades[6].classList.add("gestion");
+        habilidades[7].classList.add("comunicacion");
+        habilidades[8].classList.add("trabajo");
+        habilidades[9].classList.add("creatividad");
+        habilidades[10].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+}
+
+
+
+/*
+function ver(link) {
+    var anchor = document.createElement('a');
+    anchor.href = link;
+    anchor.click();
+  }
+
+*/ 
+//Ver retos
+function ver(link) {
+    window.location.href = link;
+  }
+  
+  // Obtener todos los elementos <a> en la página
+  var links = document.getElementsByTagName('a');
+  
+  // Recorrer todos los enlaces y asignar la función ver() a cada uno
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i].getAttribute('data-link');
+    if (link) {
+      links[i].onclick = function(event) {
+        event.preventDefault();
+        ver(this.getAttribute('data-link'));
+      };
+    }
+  }
